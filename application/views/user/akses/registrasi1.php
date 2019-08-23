4b5ad370b9065173c7dc3345a8e3de67fd548f22
bawik ardiyan <ardiyanramadhan4@gmail.com>
2019-08-23 08:00:16 +0700

    baru

---
+++ application/views/user/akses/registrasi1.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Registrasi</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="<?= base_url('assets/bower_components/bootstrap/dist/css/bootstrap.min.css'); ?>">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?= base_url('assets/bower_components/font-awesome/css/font-awesome.min.css'); ?>">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="<?= base_url('assets/bower_components/Ionicons/css/ionicons.min.css'); ?>">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?= base_url('assets/dist/css/AdminLTE.min.css'); ?>">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="<?= base_url('assets/plugins/iCheck/square/blue.css'); ?>">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+    <!-- Google Font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+
+<body class="hold-transition register-page">
+    <div class="register-box">
+        <div class="register-logo">
+            <a href="../../index2.html"><b>Registrasi</b></a>
+        </div>
+
+        <div class="register-box-body">
+            <h5 class="login-box-msg">Registrasi pengguna baru</h5>
+            <br />
+            <p style="font-family: bold">Langkah 1</p><br />
+
+            <form action="" method="post">
+                <div class="form-group has-feedback">
+                    <input type="text" class="form-control" placeholder="Nama Lengkap">
+                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                </div>
+                <div class="form-group has-feedback">
+                    <input type="text" class="form-control" placeholder="Tempat Lahir">
+                    <span class="fa fa-location-arrow form-control-feedback"></span>
+                </div>
+                <div class="form-group has-feedback">
+                    <input type="date" class="form-control" placeholder="Tanggal Lahir">
+                    <span class="fa fa-calendar form-control-feedback"></span>
+                </div>
+                <div class="form-group has-feedback">
+                    <input type="text" class="form-control" placeholder="Alamat">
+                    <span class="glyphicon glyphicon-home form-control-feedback"></span>
+                </div>
+                <div class="form-group has-feedback">
+                    <input type="text" class="form-control" placeholder="No Telepon">
+                    <span class="fa fa-phone form-control-feedback"></span>
+                </div>
+                <div class="form-group">
+                    <select class="form-control" name="jenis_kelamin">
+                        <option>jenis kelamin</option>
+                        <option value="laki-laki">Laki-laki</option>
+                        <option value="perempuan">Perempuan</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <select class="form-control" name="status">
+                        <option>Status</option>
+                        <option value="kawin">Kawin</option>
+                        <option value="belumkawin">Belum Kawin</option>
+                    </select>
+                </div>
+                <div class="row">
+                    <!-- /.col -->
+                    <div class="col-xs-4">
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">Lanjut</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+        </div>
+        <!-- /.form-box -->
+    </div>
+    <!-- /.register-box -->
+
+    <!-- jQuery 3 -->
+    <script src="<?= base_url('assets/bower_components/jquery/dist/jquery.min.js'); ?>"></script>
+    <!-- Bootstrap 3.3.7 -->
+    <script src="<?= base_url('assets/bower_components/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
+    <!-- iCheck -->
+    <script src="<?= base_url('assets/plugins/iCheck/icheck.min.js'); ?>"></script>
+    <script>
+        $(function() {
+            $('input').iCheck({
+                checkboxClass: 'icheckbox_square-blue',
+                radioClass: 'iradio_square-blue',
+                increaseArea: '20%' /* optional */
+            });
+        });
+    </script>
+</body>
+
+</html>
